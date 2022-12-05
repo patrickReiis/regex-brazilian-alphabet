5e2b735f550f1b392ff8cf81b93932e247214719
P. Reis <76563803+patrickReiis@users.noreply.github.com>
2022-12-05 11:31:16 -0300

    Create regex.js

---
+++ regex.js
@@ -0,0 +1,8 @@
+// match all letters case insentive, not strict
+/[a-zàáéóúâêôãõ]/i
+
+// match one letter case insentive, strict
+/^[a-zàáéóúâêôãõ]$/i
+
+// match one or more letters case insentive, strict
+/^[a-zàáéóúâêôãõ]+$/i
